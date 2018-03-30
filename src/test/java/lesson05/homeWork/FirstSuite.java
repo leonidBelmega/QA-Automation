db41b01e3b92d2155c3e98a731356282fddec22b
leonidBelmega <37343924+leonidBelmega@users.noreply.github.com>
2018-03-31 01:41:24 +0300

    First suite

---
+++ src/test/java/lesson05/homeWork/FirstSuite.java
@@ -0,0 +1,12 @@
+package lesson05.homeWork;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoginTest.class,
+        SearchTest.class,
+
+})
+public class FirstSuite {
+}
