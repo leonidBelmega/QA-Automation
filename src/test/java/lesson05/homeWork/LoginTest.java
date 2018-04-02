5b378b8e5e4902b3986ef9a78f163033cf3bd793
leonidBelmega <37343924+leonidBelmega@users.noreply.github.com>
2018-04-02 15:47:39 +0200

    Ascending order for tests

--- src/test/java/lesson05/homeWork/LoginTest.java
+++ src/test/java/lesson05/homeWork/LoginTest.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest {
 
     static WebDriver driver;
